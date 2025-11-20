dd880483a030613242481f2b99948f00ad606463
ArtyGeneralov <artyom1100@gmail.com>
2025-11-19 20:45:18 -0600

    Extra NoteEvents for searching, saving, content changed and a function for requesting NoteContent for editor

--- Nexus.Core/Services/NoteEvents.cs
+++ Nexus.Core/Services/NoteEvents.cs
@@ -4,10 +4,17 @@ namespace Nexus.Core.Services
 {
     public class NoteEvents
     {
+        public Func<Note, string>? RequestNoteContent;
         public event Action<Note>? NoteUpdated;
         public event Action<Note>? TopicsChanged;
+        public event Action<string>? SearchRequested;
+        public event Action<Note, string>? ContentChanged;
+        public event Action<Note, string>? SaveRequested;
 
         public void RaiseNoteUpdated(Note note) => NoteUpdated?.Invoke(note);
         public void RaiseTopicsChanged(Note note) => TopicsChanged?.Invoke(note);
+        public void RaiseSearchRequested(string query) => SearchRequested?.Invoke(query);
+        public void RaiseContentChanged(Note note, string content) => ContentChanged?.Invoke(note, content);
+        public void RaiseSaveRequested(Note note, string content) => SaveRequested?.Invoke(note, content);
     }
 }
